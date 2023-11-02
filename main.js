2a57510ae29b0c9445614ade21745e084fa89010
Shotayo Boluwatife <73708491+Uttife@users.noreply.github.com>
2023-11-02 13:34:49 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,79 +1,32 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.3/firebase-app.js';
-import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.6.3/firebase-analytics.js';
-import { getFirestore, collection, addDoc, updateDoc } from 'https://www.gstatic.com/firebasejs/9.6.3/firebase-firestore.js';
-import { getStorage, ref, uploadBytes, getDownloadURL } from 'https://www.gstatic.com/firebasejs/9.6.3/firebase-storage.js';
 
+<html>
+<head>
+    <title>Property Upload Form</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Quatr Property Upload</h1>
+        <form id="propertyForm" class="green-theme">
+            <label for="description">Description:</label>
+            <input type="text" id="description" name="description" required><br><br>
 
-// Initialize Firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyAGYKDdjNjVe13M6JUGmRNVe6PEdV8QpJY",
-    authDomain: "quatr-69067.firebaseapp.com",
-    projectId: "quatr-69067",
-    storageBucket: "quatr-69067.appspot.com",
-    messagingSenderId: "658664071138",
-    appId: "1:658664071138:web:f2eaa639b422ca087b23bb"
-};
+            <label for="rent">Rent:</label>
+            <input type="number" id="rent" name="rent" required><br><br>
 
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
+            <label for="address">Address:</label>
+            <input type="text" id="address" name="address" required><br><br>
+            <input type="file" id="image" name="image" accept="image/*" multiple><br><br>
+            <button type="submit" class="submit-button">Submit Property</button>
+        </form>
+    </div>
 
-// Get  reference to the form
-const form = document.getElementById("propertyForm");
+    <!-- Circular Progress Bar Container -->
+    <div class="circular-progress-container">
+        <div class="circular-progress" id="circularProgress"></div>
+    </div>
 
-// Add event listener for the form submission
+    <script type="module" src="main.js"></script>
 
-form.addEventListener("submit", async (e) => { // Make the event listener asynchronous
-    e.preventDefault();
-
-    // Show the overlay
-    circularProgressContainer.style.display = "flex";
-
-    const _description = form.description.value;
-    const _rent = form.rent.value;
-    const _address = form.address.value;
-    const _images = form.image.files; // Assuming single image upload
-
-    try {
-        // Ypu go do  Firebase Firestore data upload for here
-        const docRef = await addDoc(collection(db, "properties"), {
-            description: _description,
-            rent: _rent,
-            address: _address,
-            imageUrls: [], // Initialize imageUrl as an empty string
-        });
-
-        // Come upload the images to Firebase Storage
-        const _imageUrls = [];
-        for (const _image of _images){
-            const storageRef = ref(storage, `property_images/${_image.name}`);
-    
-            const snapshot = await uploadBytes(storageRef, _image);
-              // You go come collet download URL of the first uploaded image
-        const imageUrl = await getDownloadURL(storageRef);
-        _imageUrls.push(imageUrl);
-
-        }
-
-      
-        //then you go come  Update the Firestore document with the imageUrl
-        await updateDoc(docRef, {
-            imageUrls: _imageUrls,
-        });
-
-        // Hide the overlay when the process is complete
-        circularProgressContainer.style.display = "none";
-
-        // Display success messages
-        alert("Document written with id: " + docRef.id);
-        alert("Image(s) uploaded successfully");
-
-        console.log("Document written with id: ", docRef.id);
-        console.log("Image uploaded with download URLs: ", imageUrls);
-    } catch (error) {
-        console.error("Omo Error o: ", error);
-    }
-});
-
-
+</body>
+</html>
